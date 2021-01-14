ff31c8e6314ced2d19a1832b6454f0cb784384e6
FrozenBurrito <f3rm3ndouza@outlook.com>
2021-01-14 10:54:11 -0600

    Fix CORS, add morgan

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const apiRoutes = require('./routes/api_routes');
 
@@ -12,10 +13,22 @@ const app = express();
 
 connectToMongo(process.env.MONGO_URI);
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(morgan('dev'));
+
 app.use('/api/v1', apiRoutes);
+
+app.get('*', (req, res) => {
+  res.send('This is an API');
+});
 
 const PORT = process.env.PORT || 5000;
 
